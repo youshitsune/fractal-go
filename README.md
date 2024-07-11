2ee5810e4920aac455e27c57f61064edcd65c8e7
youshitsune <youshitsune@dmz.rs>
2024-07-11 06:33:10 +0200

    add server functionality

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fractal-go
 Simple fractal image generator written in Go
+
+## Installation
+Here are the commands:
+```bash
+git clone https://github.com/youshitsune/fractal-go
+cd fractal-go/
+go run main.go
+```
+
+## Usage
+With curl:
+```bash
+curl "<host>:<port>/gen/<expression>/<name>"
+```
+**name** parameter could be anything
+
+**In the future, there will be a frontend written in Python with Streamlit**
--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
     "image/png"
     "image/color"
     "github.com/Knetic/govaluate"
+    "errors"
     "fmt"
+    "net/http"
+    "github.com/labstack/echo/v4"
 )
 
-func Calc(expression *govaluate.EvaluableExpression, x, y int) uint8{
+func Calc(expression *govaluate.EvaluableExpression, x, y int) (uint8, error){
     
     parameters := make(map[string]interface{}, 8)
     parameters["x"] = x
@@ -17,25 +20,24 @@ func Calc(expression *govaluate.EvaluableExpression, x, y int) uint8{
 
     result, err := expression.Evaluate(parameters)
     if err != nil{
-        fmt.Println(err)
+        return 0, errors.New("error in calculation")
     }
-    return uint8(result.(float64))
+    return uint8(result.(float64)), nil
 }
 
-func MakeImage(expression *govaluate.EvaluableExpression, dx, dy int) [][]uint8{
+func MakeImage(expression *govaluate.EvaluableExpression, dx, dy int, name string) error{
     s := make([][]uint8, dy)
     for i:=0; i < dy; i++{
         s = append(s, make([]uint8, dx))
         for j:=0; j < dx; j++{
-            s[i] = append(s[i], Calc(expression, j, i))
+            v, err := Calc(expression, j, i)
+            if err != nil{
+                return errors.New("error in calculation")
+            } 
+            s[i] = append(s[i], v)
         }
     }
 
-    return s
-}
-
-func WriteImage(s [][]uint8){
-    dx, dy := len(s[0]), len(s)
     img := image.NewNRGBA(image.Rect(0, 0, dx, dy))
     for i:=0; i < dy; i++{
         for j:=0; j < dx; j++{
@@ -44,25 +46,35 @@ func WriteImage(s [][]uint8){
         }
     }
 
-    outputFile, err := os.Create("img")
+    outputFile, err := os.Create(name)
     if err != nil{
-        
+        return errors.New("error in writing to the file")
     }
-
     png.Encode(outputFile, img)
     outputFile.Close()
+    return nil
+}
+
+func gen(c echo.Context) error {
+    s := c.Param("exp")
+    name := c.Param("name")
+    expression, err := govaluate.NewEvaluableExpression(s)
+    if err != nil{
+        return c.String(http.StatusOK, "There is an error in expression")
+    } else {
+        err := MakeImage(expression, 256, 256, name)
+        if err != nil{
+            return c.String(http.StatusOK, fmt.Sprint(err))
+        }
+        return c.File(name)
+    }
 }
 
 func main(){
-    var x, y int
-    fmt.Scanf("%v %v", &x, &y)
-    var s string
-    fmt.Scanf("%v", &s)
-    expression, err := govaluate.NewEvaluableExpression(s)
-    if err != nil{
-        fmt.Println(err)
-    } else {
-        img := MakeImage(expression, x, y)
-        WriteImage(img)
-    }
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Just a fucking root")
+    })
+    e.GET("/gen/:exp/:name", gen)
+    e.Logger.Fatal(e.Start(":1323"))
 }
